a30d945eac66ec8dc6ba9528b11b8946b4366931
jfrianop <jfrianop@gmail.com>
2019-10-13 23:13:25 -0500

    Final Commit

--- src/app.js
+++ src/app.js
@@ -1,45 +1,89 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
 import style from './style.scss';
 import $ from 'jquery';
 import "bootstrap";
 import Handlebars from 'handlebars';
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 
-const getUsers = async () => {
-    let response = await fetch("https://makeitreal.s3.amazonaws.com/chats/users.json");
-    let jsonData = response.json();
-    return jsonData;
-}
 
-const getMessages = async (url) => {
-    let response = await fetch(url);
-    let jsonData = response.json();
-    return jsonData;
-}
+var firebaseConfig = {
+    apiKey: "AIzaSyBpH1nA_CYnYzXhDCrytir6WJNlX9oEQ5A",
+    authDomain: "topv3-chat-a1d93.firebaseapp.com",
+    databaseURL: "https://topv3-chat-a1d93.firebaseio.com",
+    projectId: "topv3-chat-a1d93",
+    storageBucket: "topv3-chat-a1d93.appspot.com",
+    messagingSenderId: "525123737831",
+    appId: "1:525123737831:web:32b04a2ea54113541f6a42",
+    measurementId: "G-0L1KY0EYX9"
+};
 
-var users = [];
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
 
 $(document).ready(() => {
-    getUsers().then(e => {
-        var source = document.getElementById("user-template").innerHTML;
-        var template = Handlebars.compile(source);
-        e.forEach((user, i) => {
-            users[i] = user;
-            var context = { name: user.name, status: user.connected ? "online" : "", id: i };
-            var html = template(context);
-            $(".user-list ul").append(html);
-        });
-    })
+    printMessages();
+})
+
+$("input").on("keypress", function (e) {
+    if (e.which == 13) {
+        sendMessage("Juan Riaño");
+    }
 });
 
-$(".user-list ul").on("click", "li", function () {
-    $(".active").removeClass("active");
-    $(this).addClass("active");
-    var id = $(this).attr("user-id")
-    getMessages(users[id].url).then(e => {
-        var source = document.getElementById("message-template").innerHTML;
-        var template = Handlebars.compile(source);
-        $(".chat").html("");
-        e.forEach((user) => {
-            $(".chat").append(template(user));
-        });
-    })
+$(".send-message").on("click", function () {
+    sendMessage("Juan Riaño");
 })
+
+const sendMessage = (name) => {
+    db.collection("messages").add({
+        sender: name,
+        message: $("input").val()
+    }).then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        $("input").val("");
+        printMessages();
+    })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+}
+
+const printMessages = () => {
+    db.collection("messages").get().then((querySnapshot) => {
+        $(".chat-messages ul").html("");
+        querySnapshot.forEach((msg) => {
+            console.log(msg.data());
+            $(".chat-messages ul").append('<li class="list-group-item message"><strong>'
+                + msg.data().sender + ': </strong>' + msg.data().message + '</li>')
+            $('.chat-messages').animate({ scrollTop: 9999 }, 'slow');
+        });
+    });
+}
+
+
+/*const ws = new WebSocket("wss://mir-chat.herokuapp.com");
+ws.onopen = function () {
+    alert("You're in")
+}
+ws.onmessage = function (msg) {
+    var obj = JSON.parse(msg.data);
+    $(".chat-messages ul").append('<li class="list-group-item message"><strong>'
+        + obj.sender + ': </strong>' + obj.message + '</li>')
+    $('.chat-messages').animate({ scrollTop: 9999 }, 'slow');
+}
+ws.onclose = function () {
+    console.log("Desconectados!");
+}
+$("input").on("keypress", function (e) {
+    console.log(e)
+    if (e.which == 13) {
+        ws.send(JSON.stringify({ sender: "Guillermo Alaix", message: $(this).val() }));
+        $(this).val("");
+        $('.chat-messages').animate({ scrollTop: 9999 }, 'slow');
+    }
+}); */
+
